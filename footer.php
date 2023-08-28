3b79bdee3022a1964324d20a8d644a6ddeb15ef9
Fvst1k <87616844+Fvst1k@users.noreply.github.com>
2023-08-29 01:18:53 +0300

    add footer

--- footer.php
+++ footer.php
@@ -1,9 +1,19 @@
 <!-- Footer -->
-<section class="footer">
-    <div class="block__content ">
 
+<footer class="footer">
+    <div class="block__content block__content_footer ">
+        <div class="footer__row">
+            <div class="footer__img">
+                <img src="" alt="">
+            </div>
+            <div class="footer__links">
+                <a href="tel:">99999999999</a>
+                <a href="mailto:">admin@admin</a>
+            </div>
+            <p class="footer__subtitle">Текст текст текст текст текст текст тексттекст текст тексттекст текст тексттекст текст тексттекст текст тексттекст текст тексттекст текст тексттекст текст тексттекст текст тексттекст текст текст</p>
+        </div>
     </div>
-</section>
+</footer>
 <!-- /Footer -->
 </div>
 
